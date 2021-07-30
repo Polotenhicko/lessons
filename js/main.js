bc7a4313e333021e5d6360a54b78d921af1ab936
Polotenhicko <artem-gribanof@yandex.ru>
2021-07-30 18:47:44 +0300

    Выполнил простое задание 2

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@ let addExpenses = '568, 1000, 761';
 let deposit = false;
 let mission = 1e6;
 let period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(', ');
+console.log(addExpenses);
+
+let budgetDay = money / 30;
+console.log(budgetDay);
